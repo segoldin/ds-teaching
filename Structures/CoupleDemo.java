7ccee30da7e0d8aa1d08cbee991f2967e9fd5dab
Sally Goldin <seg@goldin-rudahl.com>
2022-07-01 15:45:37 +0700

    formatting

--- Structures/CoupleDemo.java
+++ Structures/CoupleDemo.java
@@ -56,17 +56,17 @@ public class CoupleDemo
      */
     public static CoupleDemo findPerson(String name)
     {
-	CoupleDemo found = null;
-	CoupleDemo current = headNode;
-	while (current != null)
-	{
-	    if (current.name.equalsIgnoreCase(name))  // matched
-	    {
-		found = current;
-		break;
-	    }
-	    current = current.nextItem;
-	}
+        CoupleDemo found = null;
+        CoupleDemo current = headNode;
+        while (current != null)
+        {
+            if (current.name.equalsIgnoreCase(name))  // matched
+            {
+                found = current;
+                break;
+            }
+            current = current.nextItem;
+        }
 	return found;   // if name did not match any of the items, found will still be null
     }
     
@@ -78,91 +78,91 @@ public class CoupleDemo
      */
     public static void makeCouple(String personA, String personB)
     {
-	CoupleDemo a = findPerson(personA);
-	if (a == null)
-	{
-	    System.out.print("No person found with name " + personA);
-	    return;
-	}
-	CoupleDemo b = findPerson(personB);
-	if (b == null)
-	{
-	    System.out.print("No person found with name " + personB);
-	    return;
-	}
-	if (a == b)
-	{
-	    System.out.print("Person A and Person B are the same person!\n");
-	    return;
-	}
-	if (a.partner != null)
-	{
-	    System.out.println(a.name + " is currently partnered with " + a.partner.name + " - breaking them up");
-	    a.partner.partner = null;
-	    // if we don't do this, the old partner will still be recorded
-	    // as being in a couple
-	}
-	if (b.partner != null)
-	{
-	    System.out.println(b.name + " is currently partnered with " + b.partner.name + " - breaking them up");
-	    b.partner.partner = null;
-	    // if we don't do this, the old partner will still be recorded
-	    // as being in a couple
-	} 
-	a.partner = b;
-	b.partner = a;
-	System.out.println(a.name + " and " + b.name + " are now a couple\n");
+        CoupleDemo a = findPerson(personA);
+        if (a == null)
+        {
+            System.out.print("No person found with name " + personA);
+            return;
+        }
+        CoupleDemo b = findPerson(personB);
+        if (b == null)
+        {
+            System.out.print("No person found with name " + personB);
+            return;
+        }
+        if (a == b)
+        {
+            System.out.print("Person A and Person B are the same person!\n");
+            return;
+        }
+        if (a.partner != null)
+        {
+            System.out.println(a.name + " is currently partnered with " + a.partner.name + " - breaking them up");
+            a.partner.partner = null;
+            // if we don't do this, the old partner will still be recorded
+            // as being in a couple
+        }
+        if (b.partner != null)
+        {
+            System.out.println(b.name + " is currently partnered with " + b.partner.name + " - breaking them up");
+            b.partner.partner = null;
+            // if we don't do this, the old partner will still be recorded
+            // as being in a couple
+        } 
+        a.partner = b;
+        b.partner = a;
+        System.out.println(a.name + " and " + b.name + " are now a couple\n");
     }
 
     /** Create some initial values for the linked list
      */
     public static void initList()
     {
-	CoupleDemo newPerson = new CoupleDemo("FRANK","M");
-	headNode = tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("JENNY","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("SOMCHAI","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("ANCHALEE","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("MARK","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("JIM","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("MARY","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("ROBERT","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("SUSAN","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("WICHIAN","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("BUNDIT","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("KATE","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("LENNY","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("ARIYA","F");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	newPerson = new CoupleDemo("SAM","M");
-	tailNode.nextItem = newPerson;
-	tailNode = newPerson; peopleCount++;
-	System.out.println("There are currently " + peopleCount + " people in the list");
+        CoupleDemo newPerson = new CoupleDemo("FRANK","M");
+        headNode = tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("JENNY","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("SOMCHAI","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("ANCHALEE","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("MARK","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("JIM","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("MARY","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("ROBERT","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("SUSAN","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("WICHIAN","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("BUNDIT","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("KATE","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("LENNY","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("ARIYA","F");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        newPerson = new CoupleDemo("SAM","M");
+        tailNode.nextItem = newPerson;
+        tailNode = newPerson; peopleCount++;
+        System.out.println("There are currently " + peopleCount + " people in the list");
     }
 
 
@@ -172,9 +172,9 @@ public class CoupleDemo
      */
     public static String promptForString(String question)
     {
-	System.out.print(question +" ");
-	String input = scan.nextLine();
-	return input;
+        System.out.print(question +" ");
+        String input = scan.nextLine();
+        return input;
     }
 
 
@@ -183,87 +183,87 @@ public class CoupleDemo
      */
     public static void main(String args[])
     {
-      int option = 0;  // item chosen from menu
-      String input;  // get input from the user
-      String name1;
-      String name2;
-      String gender;
+        int option = 0;  // item chosen from menu
+        String input;  // get input from the user
+        String name1;
+        String name2;
+        String gender;
     
-      System.out.println("Welcome to Couples!");
-      initList();
-      while (option != 10)
-      {
-	  System.out.print("\n  1 -  Make a couple\n");
-	  System.out.print("  2 -  Print all couples\n");
-	  System.out.print("  3 -  Break up a couple\n");
-	  System.out.print("  4 -  Print all people's names\n");
-	  System.out.print("  5 -  Add new person\n");
-	  System.out.print("  10 - Exit\n");	
-	  input = promptForString("Enter option number:");
-	  try 
-	  {
-	      option = Integer.parseInt(input);
-	  }
-	  catch (NumberFormatException nfe)
-	  {
-	      System.out.println("Invalid integer!");
-	      continue;
-	  }
-	  if (option == 1)
-	  {
-	      name1 = promptForString("\nFirst person's name?");
-	      name2 = promptForString("Second person's name?");
-	      makeCouple(name1,name2);
-	  }
-	  else if (option == 2)
-	  {
-	      System.out.print("\nHere is the current list of couples:\n");
-	      CoupleDemo current = headNode;
-	      while (current != null)
-	      {
-		  if (current.partner != null)
-		      System.out.println(current.name + " is partnered with " + current.partner.name);
-		  current = current.nextItem;
-	      }
-	      System.out.println("[END OF LIST]\n");
-	      // Note this will print each couple twice - BONUS ASSIGNMENT... FIX THIS!
-	  }
-	  else if (option == 3)
-	  {
-	      name1 = promptForString("\nFirst person's name?");
-	      name2 = promptForString("Second person's name?");
-	      System.out.print("Not yet implemented!\n");
-	      // ASSIGNMENT ONE - Write the code here to make person A and person B NOT be 
-	      // a couple anymore. If they aren't a couple now, print a message
-	      // tell the user this.
-	      // If either name does not exist, print a message to tell the user this.
-	  }
-	  else if (option == 4)
-	  {
-	      System.out.print("\nHere is the current list of names:\n");
-	      CoupleDemo current = headNode;
-	      while (current != null)
-	      {
-		  System.out.println(current.name);
-		  current = current.nextItem;
-	      }
-	      System.out.print("[END OF LIST]\n");
-	  }
-	  else if (option == 5)
-	  {
-	      name1 = promptForString("\nNew person's name?");
-	      gender = promptForString("New person's gender?");
-	      System.out.print("Not yet implemented!\n");	    
-	      // ASSIGNMENT TWO: Write the code here to add a new person
-	      // to linked list of person objects
-	      // DON'T FORGET TO: add 1 to peopleCount 
-	      // To be completely correct, you also need to check that there
-	      // is not already a person with this name in the list.
-	  }	
-	  else if (option != 10)
-	  {
-	      System.out.print("INVALID OPTION NUMBER!\n");
-	  }
-	}
-      }
+        System.out.println("Welcome to Couples!");
+        initList();
+        while (option != 10)
+        {
+            System.out.print("\n  1 -  Make a couple\n");
+            System.out.print("  2 -  Print all couples\n");
+            System.out.print("  3 -  Break up a couple\n");
+            System.out.print("  4 -  Print all people's names\n");
+            System.out.print("  5 -  Add new person\n");
+            System.out.print("  10 - Exit\n");	
+            input = promptForString("Enter option number:");
+            try 
+            {
+                option = Integer.parseInt(input);
+            }
+            catch (NumberFormatException nfe)
+            {
+                System.out.println("Invalid integer!");
+                continue;
+            }
+            if (option == 1)
+            {
+                name1 = promptForString("\nFirst person's name?");
+                name2 = promptForString("Second person's name?");
+                makeCouple(name1,name2);
+            }
+            else if (option == 2)
+            {
+                System.out.print("\nHere is the current list of couples:\n");
+                CoupleDemo current = headNode;
+                while (current != null)
+                {
+                    if (current.partner != null)
+                    System.out.println(current.name + " is partnered with " + current.partner.name);
+                    current = current.nextItem;
+                }
+                System.out.println("[END OF LIST]\n");
+                // Note this will print each couple twice - BONUS ASSIGNMENT... FIX THIS!
+            }
+            else if (option == 3)
+            {
+                name1 = promptForString("\nFirst person's name?");
+                name2 = promptForString("Second person's name?");
+                System.out.print("Not yet implemented!\n");
+                // ASSIGNMENT ONE - Write the code here to make person A and person B NOT be 
+                // a couple anymore. If they aren't a couple now, print a message
+                // tell the user this.
+                // If either name does not exist, print a message to tell the user this.
+            }
+            else if (option == 4)
+            {
+                System.out.print("\nHere is the current list of names:\n");
+                CoupleDemo current = headNode;
+                while (current != null)
+                {
+                    System.out.println(current.name);
+                    current = current.nextItem;
+                }
+                System.out.print("[END OF LIST]\n");
+            }
+            else if (option == 5)
+            {
+                name1 = promptForString("\nNew person's name?");
+                gender = promptForString("New person's gender?");
+                System.out.print("Not yet implemented!\n");	    
+                // ASSIGNMENT TWO: Write the code here to add a new person
+                // to linked list of person objects
+                // DON'T FORGET TO: add 1 to peopleCount 
+                // To be completely correct, you also need to check that there
+                // is not already a person with this name in the list.
+            }	
+            else if (option != 10)
+            {
+                System.out.print("INVALID OPTION NUMBER!\n");
+            }
+        }
+    }   
 }
--- Structures/coupleDemo.c
+++ Structures/coupleDemo.c
@@ -39,12 +39,12 @@ int findPerson(char* name)
     int found = 0;
     while (i <= peopleCount)
     {
-	if (strcasecmp(people[i].name,name) == 0)  // matched
-	{
-	    found = i;
-	    break;
-	}
-	i = i + 1;
+        if (strcasecmp(people[i].name,name) == 0)  // matched
+        {
+            found = i;
+            break;
+        }
+        i = i + 1;
     }
     return found;   // if name did not match any of the items, found will still be 0
 }
@@ -72,8 +72,8 @@ void makeCouple(char* personA, char* personB)
     int a = findPerson(personA);
     if (a == 0)
     {
-	printf("No person found with name '%s'\n",personA);
-	return;
+        printf("No person found with name '%s'\n",personA);
+        return;
     }
     int b = findPerson(personB);
     if (b == 0)
@@ -98,7 +98,7 @@ void makeCouple(char* personA, char* personB)
     {
       	printf("%s is currently partnered with %s - breaking them up\n",
 	       people[b].name,people[people[b].partner].name);
-	people[people[b].partner].partner = 0;
+        people[people[b].partner].partner = 0;
     }
     people[a].partner = b;
     people[b].partner = a;
@@ -120,72 +120,72 @@ int main(int argc, char* argv[])
     printf("Welcome to Couples!\n");
     while (option != 10)
     {
-	printf("\n  1 -  Make a couple\n");
-	printf("  2 -  Print all couples\n");
-	printf("  3 -  Break up a couple\n");
-	printf("  4 -  Print all people's names\n");
-	printf("  5 -  Add new person\n");
-	printf("  10 - Exit\n");	
-	promptForString("Enter option number:",input);
-	sscanf(input,"%d",&option); // change to integer
-	if (option == 1)
-	{
-	    promptForString("\nFirst person's name?",name1);
-	    promptForString("Second person's name?",name2);
-	    makeCouple(name1,name2);
-	}
-	else if (option == 2)
-	{
-	    i = 1;
-	    printf("\nHere is the current list of couples:\n");
-	    while (i <= peopleCount)
-	    {
-		if ((people[i].partner != 0) && (people[i].partner > i))
-		    printf("%s is partnered with %s\n",people[i].name,
-			   people[people[i].partner].name);
-		    // the check to make sure the partner is later in the array
-		    // allows us to print each couple only once
-		i = i + 1;    
-	    }
-	    printf("[END OF LIST]\n");
-	}
-	else if (option == 3)
-	{
+        printf("\n  1 -  Make a couple\n");
+        printf("  2 -  Print all couples\n");
+        printf("  3 -  Break up a couple\n");
+        printf("  4 -  Print all people's names\n");
+        printf("  5 -  Add new person\n");
+        printf("  10 - Exit\n");	
+        promptForString("Enter option number:",input);
+        sscanf(input,"%d",&option); // change to integer
+        if (option == 1)
+        {
+            promptForString("\nFirst person's name?",name1);
+            promptForString("Second person's name?",name2);
+            makeCouple(name1,name2);
+        }
+        else if (option == 2)
+        {
+            i = 1;
+            printf("\nHere is the current list of couples:\n");
+            while (i <= peopleCount)
+            {
+                if ((people[i].partner != 0) && (people[i].partner > i))
+                    printf("%s is partnered with %s\n",people[i].name,
+                        people[people[i].partner].name);
+                // the check to make sure the partner is later in the array
+                // allows us to print each couple only once
+                i = i + 1;    
+            }
+            printf("[END OF LIST]\n");
+        }
+        else if (option == 3)
+        {
     	    promptForString("\nFirst person's name?",name1);
-	    promptForString("Second person's name?",name2);
-	    printf("Not yet implemented!\n");
-	    // ASSIGNMENT ONE - Write the code here to make person A and person B NOT be 
-	    // a couple anymore. If they aren't a couple now, print a message
-	    // tell the user this.
-	    // If either name does not exist, print a message to tell the user this.
-	}
-	else if (option == 4)
-	{
-	    i = 1;
-	    printf("\nHere is the current list of names:\n");
-	    while (i <= peopleCount)
-	    {
-		printf("%s\n",people[i].name);
-		i = i + 1;    
-	    }
-	    printf("[END OF LIST]\n");
-	}
-	else if (option == 5)
-	{
-	    promptForString("\nNew person's name?",name1);
-	    promptForString("New person's gender?",gender);
-	    printf("Not yet implemented!\n");	    
-	    // ASSIGNMENT TWO: Write the code here to add a new person
-	    // to the people array.
-	    // DON'T FORGET TO: 1) add 1 to peopleCount 
-	    //                  2) make sure the array is not full
-	    //                      (peopleCount < 30)
-	    // To be completely correct, you also need to check that there
-	    // is not already a person with this name in the array.
-	}	
-	else if (option != 10)
-	{
-	    printf("INVALID OPTION NUMBER!\n");
-	}
+            promptForString("Second person's name?",name2);
+            printf("Not yet implemented!\n");
+            // ASSIGNMENT ONE - Write the code here to make person A and person B NOT be 
+            // a couple anymore. If they aren't a couple now, print a message
+            // tell the user this.
+            // If either name does not exist, print a message to tell the user this.
+        }
+        else if (option == 4)
+        {
+            i = 1;
+            printf("\nHere is the current list of names:\n");
+            while (i <= peopleCount)
+            {
+                printf("%s\n",people[i].name);
+                i = i + 1;    
+            }
+            printf("[END OF LIST]\n");
+        }
+        else if (option == 5)
+        {
+            promptForString("\nNew person's name?",name1);
+            promptForString("New person's gender?",gender);
+            printf("Not yet implemented!\n");	    
+            // ASSIGNMENT TWO: Write the code here to add a new person
+            // to the people array.
+            // DON'T FORGET TO: 1) add 1 to peopleCount 
+            //                  2) make sure the array is not full
+            //                      (peopleCount < 30)
+            // To be completely correct, you also need to check that there
+            // is not already a person with this name in the array.
+        }	
+        else if (option != 10)
+        {
+            printf("INVALID OPTION NUMBER!\n");
+        }
     }
 }
